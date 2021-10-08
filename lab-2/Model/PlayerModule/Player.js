dffccc91bb2ca3c59497569b191de7ad0037ac61
Lucas-Tianyu <2264534468@qq.com>
2021-10-07 20:23:58 -0400

    update

--- lab-2/Model/PlayerModule/Player.js
+++ lab-2/Model/PlayerModule/Player.js
@@ -9,7 +9,7 @@
  * @author Songyuan Wu & Jing Wen
  */
 
-class Player{
+class Player {
     /**
      * Initializes a new instance of the new player
      * 
@@ -18,51 +18,51 @@ class Player{
      * 
      * @returns {Player} a new instance of a player
      */
-    constructor(name){
+    constructor(name) {
         this.player_name = name;
         this.player_score = 0;
         this.is_human = true;
     }
-     change_to_computer(){
+    change_to_computer() {
         this.is_human = false;
     }
 
     /**
-	 * update the score of the player won or lost.
-	 * 
-	 * @param {number} points - points that the player won or lost.
-	 * @returns {number} the updated score.
-	 */
-     score_add(points){
-        
+     * update the score of the player won or lost.
+     * 
+     * @param {number} points - points that the player won or lost.
+     * @returns {number} the updated score.
+     */
+    score_add(points) {
+
         this.player_score += points;
-        if (this.player_score < 0){
+        if (this.player_score < 0) {
             this.player_score = 0;
         }
         return this.player_score
     }
 
     /**
-	 * update the score of the player won or lost.
-	 * 
-	 * @param {number} score - the players's new score.
-	 * @returns {number} the new score.
-	 */
-     score_change_to(score){
-        
+     * update the score of the player won or lost.
+     * 
+     * @param {number} score - the players's new score.
+     * @returns {number} the new score.
+     */
+    score_change_to(score) {
+
         this.player_score = score;
         return this.player_score
     }
 
     /**
-	 * update the name of the player.
-	 * 
-	 * @param {string} name- the players's new name.
-	 * @returns {string} the new name.
-	 */
-     name_change_to(name){
-        
+     * update the name of the player.
+     * 
+     * @param {string} name- the players's new name.
+     * @returns {string} the new name.
+     */
+    name_change_to(name) {
+
         this.player_name = name;
         return this.player_name
     }
- }
+}
--- lab-2/Model/PlayerModule/PlayerSet.js
+++ lab-2/Model/PlayerModule/PlayerSet.js
@@ -7,192 +7,191 @@
  * @author Songyuan Wu & Jing Wen
  */
 
- class PlayerSet {
-    /**
-     * initialize the players list
-     *
-     * @returns {PlayerSet} a new instance of type Players
-     */
-    constructor() {
-      this.players_list = [];
-      this.no_of_players = 0;
-    }
-  
-    /**
-     * add a computer player to the game.
-     *
-     * @require the the player is not the game
-     * @param {number} points points that the player won or lost.
-     * @returns {ComputerPlayer} the player that just added.
-     * @returns {null} if the name is aready exsit.
-     */
-     add_computer_player(name, win_prob) {
-      if (this.player_exist(name)) {
-        return null;
-      }
-      this.no_of_players += 1;
-      var player = new ComputerPlayer(name);
-      player.win_prob = win_prob;
-      this.players_list.push(player);
-  
-      return player;
-    }
-  
-    /**
-     * add a human player to the game.
-     *
-     * @require the the player is not the game
-     * @param {string} name the name of the specific player
-     * @returns {Player} the player that just added.
-     * @returns {null} if the name is aready exsit.
-     */
-     add_player(name) {
-      if (this.player_exist(name)) {
-        return null;
-      }
-      this.no_of_players += 1;
-      var player = new Player(name);
-      this.players_list.push(player);
-  
-      return player;
-    }
-  
-    /**
-     * delete a player to the game.
-     *
-     * @require the the player is not the game
-     * @param {string} name the name of the specific player
-     * @returns {Player} the player that just deleted
-     * @returns {null} if player is not in the game
-     */
-     delete_player(name) {
-      for (var element of this.players_list) {
-        if (element.player_name == name) {
-          this.no_of_players -= 1;
-          
-          var idx=this.players_list.map(x=>x.player_name).indexOf(element.player_name)
-          this.players_list.splice(idx,1)
-          // copy_list = this.players_list;
-          // this.players_list = [];
-          // for (let i = 0; i < copy_list; i++) {
-          //   if (copy_list[i] != name) {
-          //     this.players_list.push(copy_list[i]);
-          //   }
-          // }
-          return element;
-        }
-      }
-      return null;
-    }
-  
-    /**
-     * return a specific player's score.
-     *
-     * @require the the player is in the game
-     * @param {string} name the name of the specific player
-     * @returns {Player} that player you are looking for
-     * @returns {null} if the player does not exsit.
-     */
-     players_search(name) {
-      for (var element of this.players_list) {
-        if (element.player_name == name) {
-          return element;
-        }
-      }
-      return null;
-    }
-  
-    /**
-     * reset all player's score.
-     */
-     reset_all_score() {
-      for (var element of this.players_list) {
-        element.score_change_to(0);
-      }
-    }
-  
-    /**
-     * return a specific player's score.
-     *
-     * @param {string} name the name of the specific player
-     * @returns {number} the score of one specific player
-     * @returns {null} if player does not exist.
-     */
-     players_score_check(name) {
-      for (var element of this.players_list) {
-        if (element.player_name == name) {
-          return element.player_score;
-        }
-      }
-      return null;
-    }
-  
-    /**
-     * display the highest score in this game
-     *
-     * @returns {number} the highest score
-     */
-     players_highest_score() {
-      var highest_score = 0;
-      for (var element of this.players_list) {
-        if (element.player_score > highest_score) {
-          highest_score = element.player_score;
-        }
-      }
-      return highest_score;
-    }
-  
-    /**
-     * check is the player already in the game
-     *
-     * @param {string} name the name of a specific player
-     * @returns {boolean} true the player is already in the game
-     */
-     player_exist(name) {
-      var result = false;
-      for (var element of this.players_list) {
-        console.log(element)
-        if (element.player_name == name) {
-          result = true;
-          return result;
-        }
-      }
-      return result;
-    }
+class PlayerSet {
+  /**
+   * initialize the players list
+   *
+   * @returns {PlayerSet} a new instance of type Players
+   */
+  constructor() {
+    this.players_list = [];
+    this.no_of_players = 0;
+  }
 
-    /**
-     * add the socre to a player
-     *
-     * @param {string} name the name of a specific player
-     * @param {int} score the score to add
-     * @returns {boolean} true the player is already in the game
-     */
-     add_score(name,score) {
-      var result = false;
-      for (var element of this.players_list) {
-        console.log(element)
-        if (element.player_name == name) {
-          result = true;
-          element.score_add(score);
-          return result;
-        }
-      }
-      return result;
+  /**
+   * add a computer player to the game.
+   *
+   * @require the the player is not the game
+   * @param {number} points points that the player won or lost.
+   * @returns {ComputerPlayer} the player that just added.
+   * @returns {null} if the name is aready exsit.
+   */
+  add_computer_player(name, win_prob) {
+    if (this.player_exist(name)) {
+      return null;
     }
+    this.no_of_players += 1;
+    var player = new ComputerPlayer(name);
+    player.win_prob = win_prob;
+    this.players_list.push(player);
 
-  
-    /**
-     * check is the player already in the game
-     *
-     * @param {string} name the name of a specific player
-     * @returns {boolean} true the player is already in the game
-     */
-     players_name() {
-      players = [];
-      for (var element of this.players_list) {
-        players.concat(element.player_name);
+    return player;
+  }
+
+  /**
+   * add a human player to the game.
+   *
+   * @require the the player is not the game
+   * @param {string} name the name of the specific player
+   * @returns {Player} the player that just added.
+   * @returns {null} if the name is aready exsit.
+   */
+  add_player(name) {
+    if (this.player_exist(name)) {
+      return null;
+    }
+    this.no_of_players += 1;
+    var player = new Player(name);
+    this.players_list.push(player);
+
+    return player;
+  }
+
+  /**
+   * delete a player to the game.
+   *
+   * @require the the player is not the game
+   * @param {string} name the name of the specific player
+   * @returns {Player} the player that just deleted
+   * @returns {null} if player is not in the game
+   */
+  delete_player(name) {
+    for (var element of this.players_list) {
+      if (element.player_name == name) {
+        this.no_of_players -= 1;
+
+        var idx = this.players_list.map(x => x.player_name).indexOf(element.player_name)
+        this.players_list.splice(idx, 1)
+        // copy_list = this.players_list;
+        // this.players_list = [];
+        // for (let i = 0; i < copy_list; i++) {
+        //   if (copy_list[i] != name) {
+        //     this.players_list.push(copy_list[i]);
+        //   }
+        // }
+        return element;
       }
-      return players;
+    }
+    return null;
+  }
+
+  /**
+   * return a specific player's score.
+   *
+   * @require the the player is in the game
+   * @param {string} name the name of the specific player
+   * @returns {Player} that player you are looking for
+   * @returns {null} if the player does not exsit.
+   */
+  players_search(name) {
+    for (var element of this.players_list) {
+      if (element.player_name == name) {
+        return element;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * reset all player's score.
+   */
+  reset_all_score() {
+    for (var element of this.players_list) {
+      element.score_change_to(0);
     }
   }
-  
+
+  /**
+   * return a specific player's score.
+   *
+   * @param {string} name the name of the specific player
+   * @returns {number} the score of one specific player
+   * @returns {null} if player does not exist.
+   */
+  players_score_check(name) {
+    for (var element of this.players_list) {
+      if (element.player_name == name) {
+        return element.player_score;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * display the highest score in this game
+   *
+   * @returns {number} the highest score
+   */
+  players_highest_score() {
+    var highest_score = 0;
+    for (var element of this.players_list) {
+      if (element.player_score > highest_score) {
+        highest_score = element.player_score;
+      }
+    }
+    return highest_score;
+  }
+
+  /**
+   * check is the player already in the game
+   *
+   * @param {string} name the name of a specific player
+   * @returns {boolean} true the player is already in the game
+   */
+  player_exist(name) {
+    var result = false;
+    for (var element of this.players_list) {
+      console.log(element)
+      if (element.player_name == name) {
+        result = true;
+        return result;
+      }
+    }
+    return result;
+  }
+
+  /**
+   * add the socre to a player
+   *
+   * @param {string} name the name of a specific player
+   * @param {int} score the score to add
+   * @returns {boolean} true the player is already in the game
+   */
+  add_score(name, score) {
+    var result = false;
+    for (var element of this.players_list) {
+      console.log(element)
+      if (element.player_name == name) {
+        result = true;
+        element.score_add(score);
+        return result;
+      }
+    }
+    return result;
+  }
+
+
+  /**
+   * check is the player already in the game
+   *
+   * @param {string} name the name of a specific player
+   * @returns {boolean} true the player is already in the game
+   */
+  players_name() {
+    players = [];
+    for (var element of this.players_list) {
+      players.concat(element.player_name);
+    }
+    return players;
+  }
+}
