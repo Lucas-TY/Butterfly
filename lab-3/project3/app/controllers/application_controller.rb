0fe7c7588337e62e9088b10b3ad01ae003c1d4d0
alechliter <lechliter.2@osu.edu>
2021-10-25 15:48:59 -0400

    Implemented a basic login feature using devise.
    Temporarily added the before_action :authenticate_user! to the SubjectsController to test login while no other pages exist.

--- lab-3/project3/app/controllers/application_controller.rb
+++ lab-3/project3/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    # Adds parameters to the devise routes to allow for initialization of additional attributes
+    protected def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
+    end
+
 end
