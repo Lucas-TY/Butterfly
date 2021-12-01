2b436a0c9cb6429d1ed2fbac0427313697effc90
alechliter <lechliter.2@osu.edu>
2021-12-01 16:12:17 -0500

    Fix merge errors with jing2

--- lab-4/project4/app/models/subject.rb
+++ lab-4/project4/app/models/subject.rb
@@ -1,25 +1,23 @@
 class Subject < ApplicationRecord
-    # associations
-    belongs_to :course
-    belongs_to :semester, required: false
-    belongs_to :instructor, required: false
-    has_many :plans
-    has_many :users, through: :plans
-    has_many :grading_assignments
-    has_many :graders, class_name: 'Student', through: :grading_assignments, foreign_key: "student_id"
-    
-    def self.search(search)
-        @result=[]
-        if search
-            @result=Subject.joins(:course).where(course: { course_id: search })
-            if @result==[]
-                @result=self.where(teacher: search)
-            end
-        end
-        @result
-    end
-    puts @result
-    @result
+  # associations
+  belongs_to :course
+  belongs_to :semester, required: false
+  belongs_to :instructor, required: false
+  has_many :plans
+  has_many :users, through: :plans
+  has_many :grading_assignments
+  has_many :graders, class_name: 'Student', through: :grading_assignments, foreign_key: "student_id"
+  
+  def self.search(search)
+      @result=[]
+      if search
+          @result=Subject.joins(:course).where(course: { course_id: search })
+          if @result==[]
+              @result=self.where(teacher: search)
+          end
+      end
+      @result
   end
-
+  puts @result
+  @result
 end
