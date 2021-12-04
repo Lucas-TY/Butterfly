ed82b2a3bad2d480bbaf8130b0c83a95be6d25e3
lucas <wu.4355@buckeye.osu.edu>
2021-12-04 13:31:04 -0500

    add destoy to model

--- lab-4/project4/app/models/student.rb
+++ lab-4/project4/app/models/student.rb
@@ -1,11 +1,11 @@
 class Student < ApplicationRecord
     # associations
     belongs_to :user
-    has_many :evaluations
-    has_many :recommendations
-    has_many :applications
+    has_many :evaluations,:dependent => :destroy
+    has_many :recommendations,:dependent => :destroy
+    has_many :applications,:dependent => :destroy
     has_many :taken_courses
     has_many :courses, through: :taken_courses
-    has_many :grading_assignments
+    has_many :grading_assignments,:dependent => :destroy
     has_many :sections, class_name: "Subject", foreign_key: "subject_id", through: :grading_assignments
 end
