bb19525fcb66f4deb539df891dbb17534c3d8689
lucas <wu.4355@buckeye.osu.edu>
2021-12-04 14:04:52 -0500

    skip_forgery_protection

--- lab-4/project4/app/controllers/application_controller.rb
+++ lab-4/project4/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
-    protect_from_forgery prepend: true
+    skip_forgery_protection
     # Adds parameters to the devise routes to allow for initialization of additional attributes
     protected def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
